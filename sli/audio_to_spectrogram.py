7a89895977b0330bfbcc9837ce061d2e40bb4a16
comrados <30772375+comrados@users.noreply.github.com>
2019-07-04 16:53:43 +0200

    directly to hd5

--- sli/audio_to_spectrogram.py
+++ sli/audio_to_spectrogram.py
@@ -5,16 +5,16 @@ import random
 import pandas as pd
 import os
 from PIL import Image, ImageOps
-
+import h5py
 from . import utils
 
 
 class AudioSpectrumExtractor:
 
     def __init__(self, path: str, audios: str, spec: str = "audios_spec", balanced: bool = True, n_patches: int = 10,
-                 seed: int = None, frame_length: float = 25.0, n_mels: int = 100, patch_length: float = 1.0,
-                 patch_sampling: str = 'random', save_full_spec: str = None, spec_bg='white', plotting: bool = False,
-                 verbose: bool = False):
+                 seed: int = None, frame_length: float = 25.0, patch_height: int = 100, patch_length: float = 1.0,
+                 save_as: str = 'both', patch_sampling: str = 'random', save_full_spec: str = None, invert_colors=False,
+                 plotting: bool = False, verbose: bool = False):
         """
         Initialize audio spectrogram extractor
 
@@ -25,11 +25,12 @@ class AudioSpectrumExtractor:
         :param n_patches: number of patches extracted from each spectrogram
         :param seed: seed for files' order shuffling, default - None, if None - random
         :param frame_length: spectrogram frame length in milliseconds
-        :param n_mels: quantity of spectrogram mels (spectrogram height)
+        :param patch_height: spectrogram height (quantity of spectrogram mels)
         :param patch_length: length of patch in seconds
+        :param save_as: save as images - 'img', h5 file - 'h5', both - 'both'
         :param patch_sampling: patch sampling: 'random' - random, 'gauss' - normal, 'uniform' - uniformly separated
         :param save_full_spec: path to save full spectrograms, doesn't save if None
-        :param spec_bg: spectrogram background color ('white or black')
+        :param invert_colors: spectrogram background color ('white or black')
         :param plotting: plot spectrogram and patches
         :param verbose: verbose output
         """
@@ -39,20 +40,24 @@ class AudioSpectrumExtractor:
         self.balanced = balanced
         self.n_patches = n_patches
         self.frame_length = frame_length
-        self.n_mels = n_mels
+        self.patch_height = patch_height
         self.patch_length = patch_length
         self.patch_width = int(patch_length * 1000 / self.frame_length)
+        self.save_as = save_as
         self.patch_sampling = patch_sampling
         self.seed = seed
         self.save_full_spec = save_full_spec
         if self.save_full_spec:
             utils.check_path(self.path, self.save_full_spec)
         utils.check_path(self.path, self.spec)
-        self.spec_bg = spec_bg
+        self.invert_colors = invert_colors
         self.plotting = plotting
         self.verbose = verbose
         self.spec_lang_list = []
         self.spec_full_lang_list = []
+        self.h5_file = None
+        self.h5_buffer_x = []
+        self.h5_buffer_y = []
 
     def _get_offsets(self, w):
         """wrapper for offsets acquisition"""
@@ -72,10 +77,12 @@ class AudioSpectrumExtractor:
         """
         y, sr = lr.load(file_path, sr=None)
         hop = int(sr / 1000 * self.frame_length)  # hop length (samples)
-        spec = lr.feature.melspectrogram(y=y, sr=sr, n_mels=self.n_mels, hop_length=hop)
+        spec = lr.feature.melspectrogram(y=y, sr=sr, n_mels=self.patch_height, hop_length=hop)
         db = lr.power_to_db(spec, ref=np.max)
         # rescale magnitudes: 0 to 255
-        scaled = self._scale_arr(self._normalize(db), 255).astype(np.uint8)
+        scaled = self._scale_arr(self._normalize(db))
+        if self.invert_colors:
+            scaled = 255 - scaled
         return scaled
 
     def _get_patches(self, spec):
@@ -86,13 +93,6 @@ class AudioSpectrumExtractor:
         for i in range(0, len(offsets), 2):
             patches.append(spec[:, offsets[i]:offsets[i + 1]])
         return patches, offsets
-
-    def _save_spectrogram_img(self, path, arr):
-        """save spectrogram as image"""
-        img = ImageOps.flip(Image.fromarray(arr, mode='L'))
-        if self.spec_bg == 'white':
-            img = ImageOps.invert(img)
-        img.save(path)
 
     def _save_spec(self, patch, filename, lang, num=None):
         """get name of spectrogram image and save it"""
@@ -113,18 +113,64 @@ class AudioSpectrumExtractor:
             self.spec_full_lang_list.append({'file': path, 'lang': lang})
 
     def _read_and_shuffle_df(self):
+        """read and shuffle dataframe"""
         df = pd.read_csv(self.audios)  # read
         return df.sample(frac=1, random_state=self.seed).reset_index(drop=True)  # shuffle
+
+    def _init_h5(self, max_count, h, w, lang_count):
+        """init h5 file"""
+        self.h5_file = h5py.File(os.path.join(self.path, 'data.hdf5'), 'a')
+        self.h5_file.require_dataset("x", (max_count, h, w, 1), maxshape=(max_count, h, w, 1), dtype=np.uint8)
+        self.h5_file.require_dataset("y", (max_count, lang_count), maxshape=(max_count, lang_count), dtype=np.uint8)
+
+    def _try_write_to_buffer(self, entry, lang_dummy, buff_size=1000):
+        if len(self.h5_buffer_x) == len(self.h5_buffer_y) and len(self.h5_buffer_y) < buff_size:
+            self.h5_buffer_x.append(np.flip(entry, axis=0))
+            self.h5_buffer_y.append(lang_dummy)
+            return True
+        if len(self.h5_buffer_x) != len(self.h5_buffer_y):
+            raise Exception("Something went wrong")
+        else:
+            return False
+
+    def _flush_buffer(self, count):
+        self.h5_buffer_x = np.array(self.h5_buffer_x, dtype=np.uint8)
+        self.h5_buffer_x = self.h5_buffer_x.reshape(self.h5_buffer_x.shape + (1,))
+        self.h5_buffer_y = np.array(self.h5_buffer_y)
+
+        self.h5_file["x"][count:count+len(self.h5_buffer_x)] = self.h5_buffer_x
+        self.h5_file["y"][count:count+len(self.h5_buffer_y)] = self.h5_buffer_y
+
+        count = count+len(self.h5_buffer_x)
+
+        self.h5_file["x"].flush()
+        self.h5_file["y"].flush()
+
+        self.h5_buffer_x = []
+        self.h5_buffer_y = []
+        return count
+
+    def _write_to_h5(self, entry, lang_dummy, count, force_flush=False):
+        if self._try_write_to_buffer(entry, lang_dummy) and not force_flush:
+            pass
+        else:
+            count = self._flush_buffer(count)
+            self._try_write_to_buffer(entry, lang_dummy)
+        return count
 
     def extract(self):
         """calculate spectrogram and extract patches"""
         # read files
         df = self._read_and_shuffle_df()
         threshold = self._get_min_unique_count(df)  # number of files drawn for each language (for dataset balance)
-        counts = {u: 0 for u in df['lang'].unique()}  # dict for saving counts of processed files
+        uniques = sorted(df['lang'].unique())
+        counts = {u: 0 for u in uniques}  # dict for saving counts of processed files
+        count = 0
         print("CONVERTING UP TO", df.shape[0], "FILES INTO SPECTROGRAMS")
         print("LANGUAGES:", len(counts))
         print("MIN FILES' COUNT PER LANGUAGE", threshold)
+        if self.save_as in ('both', 'h5'):
+            self._init_h5(threshold * len(counts) * self.n_patches, self.patch_height, self.patch_width, len(counts))
         for idx, row in df.iterrows():
             file_path = row['file']
             file = os.path.basename(file_path)
@@ -143,14 +189,21 @@ class AudioSpectrumExtractor:
             # get patches from spectrogram
             patches, offsets = self._get_patches(scaled)
 
-            # save spectrogram patches
-            plt.set_cmap('binary')  # grayscale colormap
-            for i, patch in enumerate(patches):
-                self._save_spec(patch, filename, lang, num=i + 1)
+            if self.save_as in ('both', 'img'):
+                # save spectrogram patches
+                plt.set_cmap('binary')  # grayscale colormap
+                for i, patch in enumerate(patches):
+                    self._save_spec(patch, filename, lang, num=i + 1)
 
-            # save full spectrogram
-            if self.save_full_spec:
-                self._save_spec(scaled, filename, lang)
+                # save full spectrogram
+                if self.save_full_spec:
+                    self._save_spec(scaled, filename, lang)
+            if self.save_as in ('both', 'h5'):
+                for patch in patches:
+                    count = self._write_to_h5(patch, self._lang_to_dummy(lang, uniques), count)
+                pass
+            else:
+                raise ("Wrong type of spectrograms saving: " + self.save_as)
 
             # output
             if idx + 1 == df.shape[0] or (idx + 1) % 100 == 0:
@@ -166,10 +219,20 @@ class AudioSpectrumExtractor:
 
         print("FILES CONVERTED TOTAL:", sum(counts.values()))
         print("FILES CONVERTED FOR EACH LANGUAGE:", counts)
-        print()
         spec_csv = utils.files_langs_to_csv(self.spec_lang_list, self.path, "audios_spec.csv")
         spec_full_csv = utils.files_langs_to_csv(self.spec_full_lang_list, self.path, "audios_spec_full.csv")
+        if self.save_as in ('both', 'h5'):
+            count = self._flush_buffer(count)
+            print("SPECTROGRAMS FLUSHED TO H5-FILE:", count)
+            self.h5_file.close()
+        print()
         return spec_csv, spec_full_csv
+
+    @staticmethod
+    def _lang_to_dummy(lang, uniques):
+        arr = np.zeros(len(uniques), dtype=np.int8)
+        arr[uniques.index(lang)] = 1
+        return arr
 
     @staticmethod
     def _get_random_offsets(w, l, n):
@@ -213,10 +276,10 @@ class AudioSpectrumExtractor:
         return offsets
 
     @staticmethod
-    def _scale_arr(arr, k):
-        """scale array values by factor k, return as int"""
-        arr = arr * k
-        return arr.astype(np.int32)
+    def _scale_arr(arr):
+        """scale array values by factor k, return as uint8"""
+        arr = arr * 255
+        return arr.astype(np.uint8)
 
     @staticmethod
     def _normalize(arr):
@@ -256,3 +319,9 @@ class AudioSpectrumExtractor:
         plt.title('Pure')
         plt.suptitle('Full spectrogram')
         plt.show()
+
+    @staticmethod
+    def _save_spectrogram_img(path, arr):
+        """save spectrogram as image"""
+        img = ImageOps.flip(Image.fromarray(arr, mode='L'))
+        img.save(path)
